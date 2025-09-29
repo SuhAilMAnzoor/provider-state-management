ad48f6bea5a2622a2d061bf18d1c2283c40896e0
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-09-29 14:24:26 +0500

    global implemention of provider in main.dart and import CounterScreen class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:provider_state_management/home_screen.dart';
-import 'package:provider_state_management/provider/counter_provider.dart';
 import 'package:provider_state_management/stateful_widget_screen.dart';
 import 'package:provider_state_management/why_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:provider_state_management/provider/screens/counter_screen.dart';
+import 'package:provider_state_management/provider/counter_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: CounterExample(),
+        home: CounterScreen(),
       ),
     );
   }
