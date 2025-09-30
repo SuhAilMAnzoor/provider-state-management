5994d337e44e77f57fb8c14969648ad9b2a84a04
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-10-01 00:00:50 +0500

    Add CounterProvider class for managing and updating state of CounterScreen with ChangeNotifer, have a add count function to icrement the counter value and NotifyListeners() Provider implemention

--- lib/provider/counter_provider.dart
+++ lib/provider/counter_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CountProvider with ChangeNotifier {
+class CounterProvider with ChangeNotifier {
   int _count = 50;
 
   int get count => _count;
